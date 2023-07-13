9c0965fbcb0a55d4351d6a32391c3844ec76ae98
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-07-13 12:10:22 +0200

    Add CRUD functionality to the UserService

---
+++ src/main/java/com/example/todo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.todo.service;
+
+
+import com.example.todo.domain.User;
+import com.example.todo.exceptions.TodoException;
+import com.example.todo.exceptions.UserException;
+
+import java.util.List;
+
+public interface UserService {
+    User addUser(User user) throws TodoException;
+    User editUser(String existingEmail, String existingUsername, String newUsername, String newEmail, String newFirstName, String newLastName) throws UserException;
+    User findUserById(Long id);
+    User findUserByEmail(String email);
+    User findUserByUsername(String username);
+    List<User> listUsers();
+    void deleteUser(Long id);
+}
