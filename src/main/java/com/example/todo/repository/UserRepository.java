ff8252d5a7aa2fc043d7671bcafe0f641da69a25
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-07-13 12:07:54 +0200

    Add userRespository

---
+++ src/main/java/com/example/todo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.todo.repository;
+
+import com.example.todo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmal(String email);
+
+    User findUserById(Long id);
+
+    User findByUsername(String username);
+}
