10347f7426ba4df5e53b618ff3137d222425165e
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-07-13 12:09:53 +0200

    Implement  CRUD functionality in TodoService

---
+++ src/main/java/com/example/todo/service/TodoServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.todo.service;
+
+import com.example.todo.domain.Todo;
+import com.example.todo.exceptions.TodoException;
+import com.example.todo.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService{
+    private final TodoRepository todoRepository;
+
+    public TodoServiceImpl(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @Override
+    public Todo addTodo(Todo todo) {
+        Todo todo1 = new Todo();
+        todo1.setDescription(todo.getDescription());
+        todo1.setTitle(todo.getTitle());
+        return todoRepository.save(todo1);
+    }
+
+    @Override
+    public Todo getTodoById(Long id) {
+        return todoRepository.getReferenceById(id);
+    }
+
+    @Override
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public Todo editTodo(Long id, String newTitle, String newDescription) throws TodoException {
+        Todo existingTodo = getTodoById(id);
+        if (existingTodo!=null){
+            existingTodo.setTitle(newTitle);
+            existingTodo.setDescription(newDescription);
+            todoRepository.save(existingTodo);
+        }else {
+            throw new TodoException("Todo does not exist");
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteTodo(Long id) {
+        todoRepository.deleteById(id);
+    }
+}
