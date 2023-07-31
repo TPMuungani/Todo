d9beb6a6aca8a95caf1fe9fedc01787522637c3f
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-07-31 18:41:43 +0200

    Update UserController.java

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.todo.domain.User;
 import com.example.todo.exceptions.TodoException;
 import com.example.todo.exceptions.UserException;
 import com.example.todo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +13,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = {"/", "/user"})
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/all")
     public ResponseEntity<List<User>> getAllUsers(){
