1921d7646d520aaa63ec5976f11697ff88166bd4
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-08-20 07:09:19 +0200

    Implement subtasksservice interface

---
+++ src/main/java/com/example/management_engine/service/SubTasksServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.management_engine.service;
+
+import com.example.management_engine.domain.SubTasks;
+import com.example.management_engine.repository.SubTasksRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.example.management_engine.enums.ProgressCheck.OPEN;
+
+@Service
+@RequiredArgsConstructor
+public class SubTasksServiceImpl implements SubTasksService{
+    private final SubTasksRepository subTasksRepository;
+    @Override
+    public SubTasks addSubtask(SubTasks subTasks) {
+        SubTasks subTasks1 = new SubTasks();
+        subTasks1.setName(subTasks.getName());
+        subTasks1.setProgressCheck(OPEN);
+        return subTasksRepository.save(subTasks1);
+    }
+
+    @Override
+    public void deleteSubTask(Long id) {
+        subTasksRepository.deleteById(id);
+    }
+
+    @Override
+    public SubTasks editASubTaskById(Long id, SubTasks subTasks) {
+        Optional<SubTasks> existingSubTask = Optional.of(subTasksRepository.findById(id).orElseThrow());
+        existingSubTask.get().setName(subTasks.getName());
+        existingSubTask.get().setProgressCheck(subTasks.getProgressCheck());
+        return subTasksRepository.save(existingSubTask.get());
+    }
+}
