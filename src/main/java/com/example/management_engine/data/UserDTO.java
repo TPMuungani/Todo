dc5b048243299fbb890c916aff7cc51326e8d725
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-08-20 06:59:22 +0200

    Add a userDTO that match User entity

---
+++ src/main/java/com/example/management_engine/data/UserDTO.java
@@ -0,0 +1,11 @@
+package com.example.management_engine.data;
+
+import com.example.management_engine.domain.Department;
+
+public class UserDTO {
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String email;
+    private Department department;
+}
