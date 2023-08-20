94e58827f3683382f4b0a0834ecdc7e838144721
Tafadzwa Muungani <109671765+TPMuungani@users.noreply.github.com>
2023-08-20 07:26:03 +0200

    Removeclasses from the old package
    All classes were replaced after modifying package from todo to management engine.

--- src/main/java/com/example/todo/exceptions/TodoException.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.exceptions;
-
-public class TodoException extends Throwable {
-    public TodoException(String message) {
-    }
-}
--- src/main/java/com/example/todo/exceptions/UserException.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.exceptions;
-
-public class UserException extends Throwable {
-    public UserException(String message) {
-    }
-}
